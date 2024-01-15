0754dcf90db9f3b0394bacf330be45ea6629ce37
Xavier Cobos <xcobos@espol.edu.ec>
2024-01-15 10:17:52 -0500

    Cambios iniciales

---
+++ RYR/src/app/tab3/tab3.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent } from  '@ionic/angular/standalone';
+
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent],
+})
+export class Tab3Page {
+  constructor() {}
+}
---
+++ RYR/src/app/tabs/tabs.page.ts
@@ -0,0 +1,25 @@
+import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { triangle, ellipse, square } from 'ionicons/icons';
+
+
+//Importe la referencia al ícono peopleCircle
+import {peopleCircle } from 'ionicons/icons';
+
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+  standalone: true,
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+})
+export class TabsPage {
+  public environmentInjector = inject(EnvironmentInjector);
+
+  constructor() {
+    addIcons({ triangle, ellipse, square });
+    addIcons({ peopleCircle });
+  }
+}
